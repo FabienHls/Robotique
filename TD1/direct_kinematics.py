676672d792dbd11f283dd2639dc46b1b0e4b7587
Fabix <fabien.haureils@etu.u-bordeaux.fr>
2016-02-02 22:10:59 +0100

    randians conversions

--- TD1/direct_kinematics.py
+++ TD1/direct_kinematics.py
@@ -5,6 +5,10 @@ def leg_dk(theta1, theta2, theta3, l1=51, l2=63.7, l3=93):
     alpha = math.radians(20.69)
     beta = math.radians(5.06)
 
+    theta1 = math.radians(theta1)
+    theta2 = math.radians(theta2)
+    theta3 = math.radians(theta3)
+
     x1 = l1 * math.cos(theta1)
     y1 = l1 * math.sin(theta1)
     z1 = 0
@@ -24,7 +28,7 @@ def leg_dk(theta1, theta2, theta3, l1=51, l2=63.7, l3=93):
 
     plan_contribution = l1 + d12 + d23
 
-    ''' x3 = (l1 + math.cos(alpha) * l2 * math.cos(theta2) +
+    x3 = (l1 + math.cos(alpha) * l2 * math.cos(theta2) +
           math.sin(theta2) * math.sin(alpha) *l2 +
           math.sin(theta3) * math.cos(beta) * l3 +
           math.cos(theta2) * math.sin(beta) * l3 * math.cos(theta1))
@@ -35,11 +39,11 @@ def leg_dk(theta1, theta2, theta3, l1=51, l2=63.7, l3=93):
     z3 = math.cos(alpha) * l2 * math.sin(theta2) \
          - math.cos(theta2) * math.sin(alpha) * l2 \
          - math.cos(theta2) * math.cos(beta) * l3 \
-         + math.sin(theta2) * math.sin(beta)* l3'''
+         + math.sin(theta2) * math.sin(beta)* l3
 
-    x3 = plan_contribution * math.cos(theta1)
+    '''x3 = plan_contribution * math.cos(theta1)
     y3 = plan_contribution * math.sin(theta1)
-    z3 = l2 * math.sin(theta2) + l3 * math.sin(theta2+theta3)
+    z3 = l2 * math.sin(theta2) + l3 * math.sin(theta2+theta3)'''
 
 
     print(x3.__round__(2))
@@ -47,4 +51,4 @@ def leg_dk(theta1, theta2, theta3, l1=51, l2=63.7, l3=93):
     print(z3.__round__(2))
 
 
-leg_dk(0,-30.645,38.501)
+leg_dk(180,-30.501,-67.819)
