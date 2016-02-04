4e5a8826d25d90f53c427963ec88f6b31aa5fb7d
Fabien Haureils <fhaureil@emi.u-bordeaux1.fr>
2016-02-04 14:07:18 +0100

    test function alkashi

--- TD2/inverse_kinematics.py
+++ TD2/inverse_kinematics.py
@@ -1,4 +1,4 @@
-import math
+from math import *
 
 
 def leg_ik(x, y, z, l1=51, l2=63.7, l3=93):
@@ -11,24 +11,30 @@ def leg_ik(x, y, z, l1=51, l2=63.7, l3=93):
     correction_theta2 = alpha
     correction_theta3 = 90 + correction_theta2 - beta
 
-    theta1 = math.atan2(y,x)
+    theta1 = atan2(y,x)
 
-    d13 = math.sqrt(y**2 + x**2) - l1
-    d = math.sqrt(z**2 + d13**2)
+    d13 = sqrt(y**2 + x**2) - l1
+    d = sqrt(z**2 + d13**2)
 
-    a = math.atan2(z,d13)
-    b = math.acos(( - (l3**2) + l2**2 + d**2) / (2 * l2 * d))
+    a = atan2(z,d13)
+    b = acos( (- (l3**2) + l2**2 + d**2) / (2 * l2 * d))
+    #b= al_kashi(l2,d,l3)
     theta2 = a + b
 
-    x = math.acos( ( - (d**2) + l3**2 + l2**2) / (2 * l2 * l3))
-    theta3 = 180 - x
+    x = acos( ( - (d**2) + l3**2 + l2**2) / (2 * l2 * l3))
+    theta3 = x + 180
 
-    theta1 = math.degrees(theta1)
-    theta2 = math.degrees(theta2) - correction_theta2
-    theta3 = math.degrees(theta3) #+ correction_theta3
+    theta1 = degrees(theta1)
+    theta2 = degrees(theta2) - correction_theta2
+    theta3 = degrees(theta3) + correction_theta3
 
+    print(b)
     print("[",theta1.__round__(3),",",theta2.__round__(3),","
         ,theta3.__round__(3),"]")
+
+def al_kashi (a, b , c):
+    return acos((b*b + c*c - a*a)/(2 * b * c))
+
 
 #Test
 leg_ik(118.79, 0, -115.14)
