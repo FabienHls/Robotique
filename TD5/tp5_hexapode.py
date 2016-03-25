b32adac29633afda5c879f634a7f18bf4677b5b4
Fabien Haureils <fhaureil@emi.u-bordeaux1.fr>
2016-03-25 19:47:50 +0100

    update tp5

--- TD5/tp5_hexapode.py
+++ TD5/tp5_hexapode.py
@@ -71,20 +71,31 @@ def leg_ik(x, y, z, l1=51, l2=63.7, l3=93):
 
     return (theta1,theta2,theta3)
 
-def envoyer_pos(x,y,z,robot,motorName):
+def envoyer_pos(x,y,z,leg):
+        angles= leg_ik(x,y,z)
+        print "angles:", angles
+        i=0
+        for m in leg:
+            m.goal_position = angles[i]
+            i=i+1
+        '''robot.goto_position(
+            {motorName+"1":angles[0],motorName+"2":angles[1],motorName+"3":angles[2]},
+            0)'''
+
+def envoyer_pos2(x,y,z,robot,motorName):
         angles= leg_ik(x,y,z)
         print "angles:", angles
         robot.goto_position(
-        	{motorName+"1":angles[0],motorName+"2":angles[1],motorName+"3":angles[2]},
-        	0)
+            {motorName+"1":angles[0],motorName+"2":angles[1],motorName+"3":angles[2]},
+            0)
 
 def envoyer_angles(theta1,theta2,theta3,robot,motorName):
-		positions = leg_dk(theta1,theta2,theta3)
-		print "positions:",positions
-		print "angles", theta1,theta2,theta3
-		robot.goto_position(
-			{motorName+"1":theta1,motorName+"2":theta2,motorName+"3":theta3},
-			0)
+        positions = leg_dk(theta1,theta2,theta3)
+        print "positions:",positions
+        print "angles", theta1,theta2,theta3
+        robot.goto_position(
+            {motorName+"1":theta1,motorName+"2":theta2,motorName+"3":theta3},
+            0)
 
 
 def recup_pos(found_ids):
@@ -95,31 +106,99 @@ def recup_pos(found_ids):
 if __name__ == '__main__':
 
 
-	with closing(pypot.robot.from_json('robotConfig.json')) as robot:
+    with closing(pypot.robot.from_json('robotConfig.json')) as robot:
 
-		# we power on the motors
-		
-		for m in robot.motors:
-			m.compliant = False #debloquer les moteurs
-			print 'Detected:', robot.motors
+        # we power on the motors
+        
+        for m in robot.motors:
+            m.compliant = False #debloquer les moteurs
+            print 'Detected:', robot.motors
 
-		for i in range(1,7):
-			time.sleep(0.5)
-			envoyer_pos(118.79, 0, -115.14,robot,"motor_"+str(i))
+        for i in range(1,7):
+            time.sleep(0.5)
+            envoyer_pos2(118.79, 0, -115.14,robot,"motor_"+str(i))
 
-		time.sleep(2)
-		'''while True:
-			t=time.time()
-			for i in range(1,7):
-				envoyer_pos(118.79, 0, -115.14+10*sin(2*pi*t*2),robot,"motor_"+str(i))'''
+        time.sleep(2)
+        '''while True:
+            t=time.time()
+            for i in range(1,7):
+                envoyer_pos(118.79, 0, -115.14+10*sin(2*pi*t*2),robot,"motor_"+str(i))'''
+
+#Placements
+        '''envoyer_angles(0, -10, -30,robot,"motor_1")
+        time.sleep(2)
+        envoyer_angles(40, 0, 0,robot,"motor_2")
+        time.sleep(2)
+        envoyer_angles(10, 0, 0,robot,"motor_3")
+        time.sleep(2)
+        envoyer_angles(0, 10, 30,robot,"motor_4")
+        time.sleep(2)
+        envoyer_angles(40, 0, 0,robot,"motor_5")
+        time.sleep(2)
+        envoyer_angles(10, 0, 0,robot,"motor_6")
+        time.sleep(2)'''
+
+        #Axe X
+        '''while True:
+            envoyer_pos(85, 0, -115.14, robot, "motor_1")
+            envoyer_pos(118.79, -20, -115.14, robot, "motor_3")
+            envoyer_pos(118.79, -20, -115.14, robot, "motor_2")
+            envoyer_pos(152.58, 0, -115.14, robot, "motor_4")
+            envoyer_pos(118.79, 20, -115.14, robot, "motor_5")
+            envoyer_pos(118.79, 20, -115.14, robot, "motor_6")
+
+            time.sleep(1)
+            envoyer_pos(152.58, 0, -115.14, robot, "motor_1")
+            envoyer_pos(118.79, 20, -115.14, robot, "motor_3")
+            envoyer_pos(118.79, 20, -115.14, robot, "motor_2")
+            envoyer_pos(85, 0, -115.14, robot, "motor_4")
+            envoyer_pos(118.79, -20, -115.14, robot, "motor_5")
+            envoyer_pos(118.79, -20, -115.14, robot, "motor_6")
+            time.sleep(1)'''
+
+        '''while True:
+                    envoyer_pos(118.79, -20, -115.14, robot, "motor_1")
+                    envoyer_pos(85, 0, -115.14, robot, "motor_2")
+                    envoyer_pos(85, 0, -115.14, robot, "motor_3")
+                    envoyer_pos(118.79, 20, -115.14, robot, "motor_4")
+                    envoyer_pos(152.58, 0, -115.14, robot, "motor_5")
+                    envoyer_pos(152.58, 0, -115.14, robot, "motor_6")
+
+                    time.sleep(1)
+                    envoyer_pos(118.79, 20, -115.14, robot, "motor_1")
+                    envoyer_pos(152.58, 0, -115.14, robot, "motor_2")
+                    envoyer_pos(152.58, 0, -115.14, robot, "motor_3")
+                    envoyer_pos(118.79, -20, -115.14, robot, "motor_4")
+                    envoyer_pos(85, 0, -115.14, robot, "motor_5")
+                    envoyer_pos(85, 0, -115.14, robot, "motor_6")
+                    time.sleep(1)'''
 
 
-		envoyer_angles(-50, 0, 0,robot,"motor_3")
-		time.sleep(2)
-		envoyer_angles(-50, 0, 0,robot,"motor_1")
+        while True: 
+                    envoyer_pos(152.58, 0, -115.14, robot.leg6)
+                    envoyer_pos(152.58, 0, -115.14, robot.leg5)
+                    envoyer_pos(118.79, 20, -115.14,robot.leg4)
+                    envoyer_pos(85, 0, -115.14,robot.leg3)
+                    envoyer_pos(85, 0, -115.14,robot.leg2)
+                    envoyer_pos(118.79, -20, -115.14, robot.leg1)
 
-		# we power off the motors
-		time.sleep(10)  # we wait for 1s
-		for m in robot.motors:
-			m.compliant=True #rebloquer moteurs
-		time.sleep(1)  # we wait for 1s
+                    time.sleep(1)
+                    envoyer_pos(85, 0, -115.14, robot.leg6)
+                    envoyer_pos(85, 0, -115.14, robot.leg5)
+                    envoyer_pos(118.79, -20, -115.14, robot.leg4)
+                    envoyer_pos(152.58, 0, -115.14, robot.leg3)
+                    envoyer_pos(152.58, 0, -115.14, robot.leg2)
+                    envoyer_pos(118.79, 20, -115.14, robot.leg1)                    
+                    time.sleep(1)
+
+
+
+
+
+
+      
+        # we power off the motors
+        time.sleep(30)  # we wait for 1s
+        for m in robot.motors:
+            m.compliant=True #rebloquer moteurs
+        time.sleep(1)  # we wait for 1s
